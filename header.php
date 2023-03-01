406ad317e074c43aeba23881df9319d298d158e6
Adam Grover <adamggrover@gmail.com>
2023-03-01 14:48:53 +0000

    remove google tag

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Weare Academy First School and Rainbow Woods Pre-School</title>
+    <title>Wessex Learning Trust</title>
     <link rel="icon" type="image/x-icon" href="Assets/logos/kings-icon.svg">
 
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B0NFPDKC8G"></script>
+    
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
