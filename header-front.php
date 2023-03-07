36995b3313f118434edd1b22fe7b81ea57551a4b
Adam Grover <adamggrover@gmail.com>
2023-03-07 16:24:20 +0000

    Update hero arrow link

--- header-front.php
+++ header-front.php
@@ -135,7 +135,7 @@
 
     
         <div class="hero-arrow-container text-center">
-            <a class="hero-arrow " href="#welcome"><i class="fa-solid fa-circle-arrow-down fa-2x animate__animated animate__fadeIn animate__delay-2s animate__slower"></i></a>
+            <a class="hero-arrow " href="#welcome"><div class="hero-arrow-box animate__animated animate__fadeIn animate__delay-2s animate__slower"><i class="fa-solid fa-arrow-down fa-2x hero-arrow-icon"></i></div></a>
         </div>
 
     
